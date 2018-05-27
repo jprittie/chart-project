75eaaff33b872a234664b88f2902b9ec06943515
jprittie <eyewallop@gmail.com>
2018-05-27 08:09:08 -0400

    Adds react table

--- app/src/components/HourlyStatsTable.js
+++ app/src/components/HourlyStatsTable.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import './ChartContainer.css';
+
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
+import './HourlyStatsTable.css';
 
 import { getHourlyStats } from '../redux/selectors';
 import { hourlyStatsApiRequest } from '../redux/actions/chart.actions.js';
+
+// TODO change timestamp to date to be user-friendly, change revenue to two decimal places
+// do this in the selector
+// make API call to get page count
 
 class HourlyStatsTable extends React.Component {
   componentDidMount () {
@@ -12,11 +19,35 @@ class HourlyStatsTable extends React.Component {
 
   render () {
     const { hourlyStats } = this.props;
+    const columns = [{
+      Header: 'Date',
+      accessor: 'date'
+    }, {
+      Header: 'Hour',
+      accessor: 'hour'
+    }, {
+      Header: 'Impressions',
+      accessor: 'impressions'
+    }, {
+      Header: 'Clicks',
+      accessor: 'clicks'
+    }, {
+      Header: 'Revenue',
+      accessor: 'revenue'
+    }];
+
     return (
-      <div>
-        This is a placeholder for the hourly stats table.
+      <div className='HourlyStatsTable-container'>
         { hourlyStats &&
-          hourlyStats.map(data => <li>{data.clicks}</li>)
+          <ReactTable
+            className='-striped -highlight'
+            data={hourlyStats}
+            columns={columns}
+            showPageSizeOptions={false}
+            pages={42}
+            manual
+            onPageChange={pageIndex => this.props.hourlyStatsApiRequest({page: pageIndex + 1, page_size: 20})}
+          />
         }
       </div>
     );
